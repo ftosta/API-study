16b04c7f7275adb5aa84dd7e8a9cc845af35e15d
Fernando Tosta <tosta.fernando@gmail.com>
2020-09-06 01:01:46 -0300

    Removed "repositories" files
    Don't make sense, models can connect directly to database.handler

--- src/handlers/driver/test.driver.js
+++ src/handlers/driver/test.driver.js
@@ -41,6 +41,43 @@ class DatabaseDevelopment {
         return this.status
     }
 
+    async find() {
+        try {
+            const conn = await this.conn()
+            return conn.getResponseBody()
+        } catch (eRepositoryInterfaceFind) {
+            throw eRepositoryInterfaceFind
+        }
+    }
+
+    async findOne(_id) {
+        try {
+            const conn = await this.conn()
+            const data = conn.getResponseBody()
+            const response = data.shift()
+
+            return response
+        } catch (eRepositoryInterfaceFindOne) {
+            throw eRepositoryInterfaceFindOne
+        }
+    }
+
+    async create(data) {
+        try {
+            const allRows = await this.find()
+            const allRowsWithNew = allRows.concat(data)
+ 
+            return allRowsWithNew
+        } catch (eRepositoryInterfaceCreate) {
+            throw eRepositoryInterfaceCreate
+        }
+    }
+
+    randomId() {
+        const rnd = Math.floor(Math.random() * 10) + 1;
+        return rnd -1 // from 0 to 9
+    }
+
 }
 
 module.exports = DatabaseDevelopment
